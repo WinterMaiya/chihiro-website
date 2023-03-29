df7c36dfa8db2755a5fc09305c472f90d84dd602
Maiya Winter <wintermaiya@gmail.com>
2023-03-29 12:15:41 -0600

    Improving quote generator

--- src/Quote of the Day/quoteGenerator.js
+++ src/Quote of the Day/quoteGenerator.js
@@ -2,18 +2,55 @@ import axios from "axios";
 import randomNum from "./randomNum";
 
 const quoteGenerator = async () => {
-	// Generates a new quote of the day for Chihiro. This will be in mostly meows as a joke with some punctuality.
+	// Generates a new quote of the day for Chihiro. This will be in mostly meows as a joke with some punctuality. This uses a quote of the day api and then replaces the words
 	/* 
     Meow meow meow, meow meow!
     Meow, Meow, Meow.
     Meow, Mow, mow meow.
     */
 
-	const numberOfSentences = randomNum(4);
-	// This determines the number of periods in a sentence
-	console.log(numberOfSentences);
+	const whichCatNoise = (word) => {
+		// Takes the first character of the word to determine what cat sound should be made
+		// All cat sounds meow, mow, mrow, mrrr, prr, prrt, hh, mrrrrrrrrrrawr, mew, rrrr, e-e-e-e-e-e, awr, hiiiiiiiiiss, mEEEwr, aaaaaaaaaaaa, Wrrrao, Maaorraa
+		let letter = word[0].toLowerCase();
+		switch (letter) {
+			case "z":
+				return "maaaaaaaa";
+			case "f":
+				return "wrrrao";
+			case "j":
+				return "maaorraa";
+			case "q":
+				return "hiiiiiiiiiiisss";
+			case "a":
+				return "mow";
+			case "h":
+				return "mrow";
+			case "i":
+				return "mrrr";
+			case "b":
+				return "prr";
+			case "d":
+				return "prrr";
+			case "h":
+				return "mrrrrrrrrrrawr";
+			case "r":
+				return "mew";
+			case "p":
+				return "rrr";
+			case "u":
+				return "et-et-et-et";
+			case "s":
+				return "awr";
+			case "n":
+				return "meeow";
+			default:
+				return "meow";
+		}
+	};
 
 	const getQuoteApi = async () => {
+		// Connects to api endpoint https://quotes.rest which generates a quote of the day
 		try {
 			const { data } = await axios.get("https://quotes.rest/qod?language=en");
 			// console.log(data);
@@ -23,30 +60,43 @@ const quoteGenerator = async () => {
 			return "error";
 		}
 	};
+
+	// Converts quotes into an array of strings
 	const quote = await getQuoteApi();
-	console.log(quote.split(" ").join(" "));
 	let arrQuote = quote.split(" ");
-	const findEndPunctuation = (word) => {
-		// TODO
-	};
+
 	// Determines if the next word needs to be capitalized
 	let capitalizeNext = true;
+
 	for (let index in arrQuote) {
 		let word = arrQuote[index];
 		let endingPunc = "";
 		let toCap = false;
 
+		// Checks any ending punctuation
 		if (word.slice(-1) === ".") {
 			toCap = true;
 			endingPunc += ".";
 		}
-		// Simple function
-		if (capitalizeNext) {
-			arrQuote[index] = "Meow" + endingPunc;
-			capitalizeNext = false;
-		} else {
-			arrQuote[index] = "meow" + endingPunc;
+		if (word.slice(-3) === "...") {
+			// Specifically if there are trailing dots
+			endingPunc += "..";
 		}
+		if (word.slice(-2) === "'s") {
+			endingPunc += "'s";
+		}
+		if (word.slice(-1) === ",") {
+			endingPunc += ",";
+		}
+
+		word = whichCatNoise(word) + endingPunc;
+		if (capitalizeNext === true) {
+			// Capitalizes a word if it needs to be
+			word = word.charAt(0).toUpperCase() + word.slice(1);
+		}
+
+		// Replaces the word in the index
+		arrQuote[index] = word;
 
 		// Decides if the next letter should be capital
 		toCap === true ? (capitalizeNext = true) : (capitalizeNext = false);
