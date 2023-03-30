2eb3329c30b8c2a6b4b8eceebc8a890d2a6bb81b
Maiya Winter <wintermaiya@gmail.com>
2023-03-30 12:50:07 -0600

    Adding parallax feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Parallax from "./components/Parallax";
 import QuoteOfTheDay from "./components/QuoteOfTheDay";
 
 function App() {
 	// TODO create components for website
 	return (
 		<div className="App">
+			<h1>MEET CHIHIRO WINTER</h1>
 			<QuoteOfTheDay />
+			<Parallax>
+				<h2>test</h2>
+			</Parallax>
+			<Parallax>
+				<h2>Test</h2>
+			</Parallax>
 		</div>
 	);
 }
---
+++ src/components/Parallax.jsx
@@ -0,0 +1,5 @@
+const Parallax = ({ children }) => {
+	return <div class="parallax parallax-image-1">{children}</div>;
+};
+
+export default Parallax;
